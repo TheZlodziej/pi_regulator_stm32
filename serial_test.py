ec92d16bb84a5908871397e519ae743ed58efab0
Jakub Wicher <galaxykuby@gmail.com>
2023-01-16 20:04:55 +0100

    testowanie odbierania i wysylania (zmiany parametrow) danych

---
+++ serial_test.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+# Open the serial port
+ser = serial.Serial('COM3', 115200, parity=serial.PARITY_NONE)
+
+uref = 20
+ti = 0.05
+kp = 1
+kc = 1
+
+while True:
+    uref += 0.2
+
+    line_read = ser.readline().decode().strip()
+    print(line_read)
+
+    line_sent = f'{{"Kc":{kc}, "Kp":{kp}, "Ti":{ti}, "Uref":{uref}}}\r\n'
+    ser.write(bytes(line_sent, 'ascii'))
+    print(line_sent)
+
+    time.sleep(1)
